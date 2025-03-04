df93904b6d0f2bec95a21a1cc4ba2de2c9353404
Sanskar Chirania <sanskar125112@gmail.com>
2025-03-04 12:02:01 +0530

    Add Google Maps API key to api.js configuration

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 var API="https://ttsgps.in/getandrojsonforNewIphone.jsp?";
 var OLA_KEY='DBfNWlRlKq4gPH2NlXQ0s3EvHJEknyX9IVHzg96e';
 var MMI_KEY='';
-var GOOGLE_KEY='';
+var GOOGLE_KEY='AIzaSyBjSJzY7GgAuRw_Pf5tUCJU7oMm_rg0aWg';
