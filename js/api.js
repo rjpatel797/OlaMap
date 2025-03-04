99127dd51ce4b10dd748f9e021824db03d6f45ad
Sanskar Chirania <sanskar125112@gmail.com>
2025-03-04 13:09:59 +0530

    Update API endpoint and POI data handling
    - Changed API base URL in api.js to new server endpoint
    - Modified PointOfInterest.html to handle nested data structure from API response
    - Updated marker and table rendering to use `data.data` instead of direct array
    - Improved data parsing and display logic for POI list

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 
-var API="https://ttsgps.in/getandrojsonforNewIphone.jsp?";
+var API="http://144.76.96.154:8485";
 var OLA_KEY='DBfNWlRlKq4gPH2NlXQ0s3EvHJEknyX9IVHzg96e';
 var MMI_KEY='';
 var GOOGLE_KEY='AIzaSyBjSJzY7GgAuRw_Pf5tUCJU7oMm_rg0aWg';
